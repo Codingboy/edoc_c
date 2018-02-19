26fef210a795408750d35278ee20eee54e0f9111
Daniel Tkocz <daniel.tkocz@gmx.de>
2018-02-19 14:25:27 +0100

    normal build

--- src/edoc.c
+++ src/edoc.c
@@ -52,7 +52,7 @@ int main(int argc, char* argv[])
 		int fileLength = strlen(file);
 		int outFileLength = fileLength+5;
 		char outFile[outFileLength+1];
-		strncpy(outFile, file, fileLength);
+		strncpy(outFile, file, fileLength+1);
 		strncat(outFile, ".edoc", 5);
 printf("%s\n", outFile);
 	}
